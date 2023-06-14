777c6bb5b3f4c17188f716768ae4e7eaa1b4e417
haruza123 <124354652+haruza123@users.noreply.github.com>
2023-06-14 09:59:51 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./App";
+import AboutUs from "./AboutUs/MainAbout";
+import Favorite from "./Favorite";
+import Album from "./Album";
+import { persistor, store } from "./Redux";
+import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/album/:id" element={<Album />} />
+        </Routes>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
